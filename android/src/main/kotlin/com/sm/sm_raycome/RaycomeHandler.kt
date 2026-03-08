1c53a3938a87151281590599f29771806046ea29
walee <waleedalmadhab@gmail.com>
2026-03-08 15:01:29 +0300

    fix rxjava

--- android/src/main/kotlin/com/sm/sm_raycome/RaycomeHandler.kt
+++ android/src/main/kotlin/com/sm/sm_raycome/RaycomeHandler.kt
@@ -4,36 +4,47 @@ import com.sm.sm_raycome.iknetbluetoothlibrary.BluetoothManager
 import com.sm.sm_raycome.iknetbluetoothlibrary.MeasurementResult
 import android.bluetooth.BluetoothDevice
 import io.flutter.plugin.common.EventChannel
-import android.os.Handler
-import android.os.Looper
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.disposables.Disposable
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class RaycomeHandler : EventChannel.StreamHandler, BluetoothManager.OnBTMeasureListener {
     private var eventSink: EventChannel.EventSink? = null
-    private val mainHandler = Handler(Looper.getMainLooper())
     private val TAG = "RaycomeHandler"
+
+    // Subject to handle measurement events reactively
+    private val eventSubject = PublishSubject.create<Map<String, Any?>>()
+    private var disposable: Disposable? = null
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
         android.util.Log.i(TAG, "onListen called - Flutter is now listening for events")
         eventSink = events
+
+        // Subscribe to the event stream and deliver to Flutter on the main thread
+        disposable = eventSubject
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ event ->
+                eventSink?.success(event)
+            }, { error ->
+                android.util.Log.e(TAG, "RxJava stream error: ${error.message}")
+                eventSink?.error("RX_ERROR", error.message, null)
+            })
     }
 
     override fun onCancel(arguments: Any?) {
         android.util.Log.i(TAG, "onCancel called - Flutter stopped listening")
+        disposable?.dispose()
+        disposable = null
         eventSink = null
     }
 
     private fun sendEvent(type: String, data: Any?) {
-        mainHandler.post {
-            if (eventSink == null) {
-                android.util.Log.w(TAG, "sendEvent FAILED - eventSink is null! Type: $type, Data: $data")
-            } else {
-                android.util.Log.i(TAG, "sendEvent: type=$type, data=$data")
-            }
-            val event = mutableMapOf<String, Any?>()
-            event["type"] = type
-            event["data"] = data
-            eventSink?.success(event)
-        }
+        val event = mutableMapOf<String, Any?>()
+        event["type"] = type
+        event["data"] = data
+        
+        // Emit event to the reactive subject
+        eventSubject.onNext(event)
     }
 
     override fun onFoundFinish(deviceList: List<BluetoothDevice>?) {
